741ef3844d30527275308608c9a9ae7a91df7844
Emmanuel8code <casemmanuel8@gmail.com>
2021-08-27 11:02:08 -0300

    Add interface and service to use jwt token

---
+++ Infrastructure/Security/JwtService.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Interfaces.ISecurity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Security
+{
+    public class JwtService : ITokenClaimsService
+    {
+        public Task<string> GetTokenAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
